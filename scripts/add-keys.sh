b13159d3078670c45f8277c0385a89f3e9a0c529
Eugene Yemelin <evgeniy.yemelin@gmail.com>
2024-12-07 08:44:19 +0300

    Separate ci stages

--- scripts/add-keys.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir ~/.ssh/
-touch ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-echo $1 > ~/.ssh/id_rsa
-ssh-keyscan -t rsa -H $2 >> ~/.ssh/known_hosts
-echo "Host $2\n  AddKeysToAgent yes\n  IdentityFile ~/.ssh/id_rsa\n" > ~/.ssh/config
--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ssh github_runner@$1 "sudo systemctl stop go-web-site"
-scp -r ~/build github_runner@${{ secrets.SSH_HOST }}:~
-chmod -R +x ~/build
-ssh github_runner@$1 "sudo systemctl start go-web-site"
